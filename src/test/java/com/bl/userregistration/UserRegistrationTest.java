73a845cc0f682cad707ccc3cc9b7de32a3c8bbe0
unknown <altamashbagwan1@gmail.com>
2021-10-28 22:53:17 +0530

    Refactored: change name in test phone number to password

--- src/test/java/com/bl/userregistration/UserRegistrationTest.java
+++ src/test/java/com/bl/userregistration/UserRegistrationTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class UserRegistrationTest {
 
     @Test
-    public void givenPhoneNumber_WhenCorrect_ShouldReturnTrue() {
+    public void givenPassword_WhenCorrect_ShouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
         boolean userTest = userRegistration.passwordTest("altamash");
         Assert.assertTrue(userTest);
