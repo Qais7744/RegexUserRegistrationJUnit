daeccd0c2723ff3a5cf0d57c755992e3669ae86a
unknown <altamashbagwan1@gmail.com>
2021-10-28 21:14:29 +0530

    Added: Functioning of First Name minimum three character in JUnitTest

--- src/test/java/com/bl/userregistration/UserRegistrationTest.java
+++ src/test/java/com/bl/userregistration/UserRegistrationTest.java
@@ -8,7 +8,7 @@ public class UserRegistrationTest {
     @Test
     public void givenFirstNameWhenShouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean userTest = userRegistration.firstName("Altamash");
+        boolean userTest = userRegistration.firstName("Qais");
         Assert.assertTrue(userTest);
     }
 }
