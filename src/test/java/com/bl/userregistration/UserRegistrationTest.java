1cc08628690e794dcb0bdf67bafa9c21edb39cab
unknown <altamashbagwan1@gmail.com>
2021-10-28 23:21:07 +0530

    Added: Functioning of Enter Any Email to Check valid or non valid in JUnit

--- src/test/java/com/bl/userregistration/UserRegistrationTest.java
+++ src/test/java/com/bl/userregistration/UserRegistrationTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class UserRegistrationTest {
 
     @Test
-    public void givenAnyEmail_WhenValid_ShouldReturnTrue() {
+    public void givenAnyEmail_WhenValidOrNonValid_ShouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
         boolean userTest = userRegistration.emailTest("abc+100@gmail.com");
         Assert.assertTrue(userTest);
