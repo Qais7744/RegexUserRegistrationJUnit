db386bcc0ac5ba7dbb324830468633989fd98034
unknown <altamashbagwan1@gmail.com>
2021-10-28 21:37:15 +0530

    Added: Functioning of Enter Mobile Number Format in JUnitTest

--- src/main/java/com/bl/userregistration/UserRegistration.java
+++ src/main/java/com/bl/userregistration/UserRegistration.java
@@ -3,10 +3,10 @@ package com.bl.userregistration;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-    public static final String EMAIL_PATTERN = "^[A-Za-z._]+[@][a-zA-Z]+[.a-z]{4}([.a-zA-Z]{3})*$";
+    public static final String PHONE_NUMBER_PATTERN = "^[0-9]{2}[ ][789][0-9]{9}$";
 
-    public boolean email(String gmail) {
-        boolean matchesString = Pattern.matches(EMAIL_PATTERN, gmail);
+    public boolean phoneNumber(String contactNumber) {
+        boolean matchesString = Pattern.matches(PHONE_NUMBER_PATTERN, contactNumber);
         return matchesString;
     }
 }
