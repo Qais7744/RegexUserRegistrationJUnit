97a39bf4d2afa58d7edea4c33b0423d057c7a8dd
unknown <altamashbagwan1@gmail.com>
2021-11-12 23:27:53 +0530

    Refactored: change String name

--- src/main/java/com/bl/userregistration/UserRegistration.java
+++ src/main/java/com/bl/userregistration/UserRegistration.java
@@ -3,23 +3,23 @@ package com.bl.userregistration;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-    public static boolean validateDetails(String guysWithMe, String matcherWithRejex, String pattern) {
+    public static boolean validateDetails(String validate, String matcherWithRejex, String pattern) {
 
         switch (pattern) {
             case "EMAIL_PATTERN":
-                boolean forTestEmail = Pattern.matches(guysWithMe, matcherWithRejex);
+                boolean forTestEmail = Pattern.matches(validate, matcherWithRejex);
                 return forTestEmail;
             case "FIRST_NAME_PATTERN":
-                boolean forTestFirstName = Pattern.matches(guysWithMe, matcherWithRejex);
+                boolean forTestFirstName = Pattern.matches(validate, matcherWithRejex);
                 return forTestFirstName;
             case "LAST_NAME_PATTERN":
-                boolean forTestLastName = Pattern.matches(guysWithMe, matcherWithRejex);
+                boolean forTestLastName = Pattern.matches(validate, matcherWithRejex);
                 return forTestLastName;
             case "MOBILE_NO_PATTERN":
-                boolean forTestmobileNumber = Pattern.matches(guysWithMe, matcherWithRejex);
+                boolean forTestmobileNumber = Pattern.matches(validate, matcherWithRejex);
                 return forTestmobileNumber;
             case "PASSWORD_PATTERN":
-                boolean forTestPassword = Pattern.matches(guysWithMe, matcherWithRejex);
+                boolean forTestPassword = Pattern.matches(validate, matcherWithRejex);
                 return forTestPassword;
             default:
                 System.out.println("invalid input");
